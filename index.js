80ce8f2448e2f4b9a4182a0ea8ae34041d84de67
daniswara2020-source <daniswara2020@gmail.com>
2026-01-28 17:42:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const LINE_CONFIG = {
 };
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
-const SHEET_NAME = "Form_Responses 1";
+const SHEET_NAME = "'Form_Responses 1'";
 const KEYWORD = "ORDER";
 
 /* ========= LINE ========= */
